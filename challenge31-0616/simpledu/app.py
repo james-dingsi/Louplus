1ce3bd4c97a16583efd8a50ce3e4caa9a2a70635
james-dingsi <60849024+james-dingsi@users.noreply.github.com>
2020-06-17 11:27:30 +0800

    Update app.py
    pull flow

--- challenge31-0616/simpledu/app.py
+++ challenge31-0616/simpledu/app.py
@@ -20,11 +20,12 @@ def register_extensions(app):
     login_manager.login_view = 'front.login'
 
 def register_blueprints(app):
-    from .handlers import front, course, admin, user
+    from .handlers import front, course, admin, user, live
     app.register_blueprint(front)
     app.register_blueprint(course)
     app.register_blueprint(admin)
     app.register_blueprint(user)
+    app.register_blueprint(live)
 
 '''app工厂函数  '''
 def create_app(config):
