57d3ea1b9a2f417da7fd748bac68414efb527c64
james-dingsi <332439161.com>
2020-06-19 18:49:43 +0800

    delete dir

--- challenge33-34/simpledu/handlers/live.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from simpledu.models import Live
-live = Blueprint('live', __name__, url_prefix='/live')
-
-@live.route('/')
-def index():
-	live = Live.query.first()
-	return render_template('live/index.html', live=live)
-  
